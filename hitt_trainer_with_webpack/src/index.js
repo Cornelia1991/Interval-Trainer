2d3a9a2c717543ab32d73bcd03cf771ebbc433d6
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-02 14:37:50 -0400

    added code for modal

--- hitt_trainer_with_webpack/src/index.js
+++ hitt_trainer_with_webpack/src/index.js
@@ -2,5 +2,14 @@ import './style.css';
 import {loadEventListeners} from './workout_entry.js';
 import {workoutPage} from './workout_timer.js';
 
+//for the suggeations modal
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+//used for workout_entry.js
 loadEventListeners();
+
+//used for workout_timer.js
 workoutPage();
